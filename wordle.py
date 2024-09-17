2a22ddc6dbf6d6aa2f577423690fc22bfe295ef1
Norah Kim <norahk1049@gmail.com>
2024-09-17 17:46:28 +0000

    message

--- wordle.py
+++ wordle.py
@@ -1,4 +1,11 @@
-word = "awesome"
+
+import random
+possible_words = ["great","swift","slime","break","quake"]
+
+word = random.choice(possible_words)
+
+
+word = "great"
 
 # colors for printing
 default = '\033[0m'
@@ -11,13 +18,14 @@ def generate_hint (user_guess):
     hint = ""
     for i in range(5):
         if (user_guess[i] == word [i]):
-        color = green
+            color = green
         elif (user_guess[i] in word):
             color = yellow
         else: 
             color = default
-
-    return user_guess
+            
+        hint = hint+color+user_guess[i]+default 
+    return hint 
 
 
 def game_loop ():
@@ -25,7 +33,7 @@ def game_loop ():
     guess = ""
     for i in range (6):
         guess = input ("what is your guess: ")
-        print (generate_guess)
+        print (generate_hint(guess))
         if (guess == word):
             print ("congratulations!!!")
             break
